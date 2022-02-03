039f95cc9da63f373fd1f89975a2f2f54e8d6bc8
iinsue <hinsu93@naver.com>
2022-02-03 14:08:21 +0900

    3.2 GET Requests

--- src/server.js
+++ src/server.js
@@ -4,7 +4,11 @@ const PORT = 4000;
 
 const app = express();
 
+const handleHome = () => console.log("Somebody is trying to go home.");
+
+app.get("/", handleHome);
+
 const handleListening = () =>
-  console.log(`Server listening on port ${PORT} 🚀`);
+  console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
 
 app.listen(PORT, handleListening);
