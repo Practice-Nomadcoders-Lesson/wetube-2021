1dac8bae002c2f68270286b6a0727179f50cb897
iinsue <hinsu93@naver.com>
2022-02-03 14:41:34 +0900

    3.3 Responses

--- src/server.js
+++ src/server.js
@@ -4,9 +4,16 @@ const PORT = 4000;
 
 const app = express();
 
-const handleHome = () => console.log("Somebody is trying to go home.");
+const handleHome = (req, res) => {
+  return res.end();
+};
+
+const handleLogin = (req, res) => {
+  return res.send("Login here.");
+};
 
 app.get("/", handleHome);
+app.get("/login", handleLogin);
 
 const handleListening = () =>
   console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
