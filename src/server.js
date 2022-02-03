c829eaa1cc958dcb641a6f198864ae31a09404e2
iinsue <hinsu93@naver.com>
2022-02-03 17:07:01 +0900

    middleware 예시 제거

--- src/server.js
+++ src/server.js
@@ -9,28 +9,11 @@ const logger = (req, res, next) => {
   next();
 };
 
-const privateMiddleware = (req, res, next) => {
-  const url = req.url;
-  if (url === "/protected") {
-    return res.send("<h1>Not Allowed</h1>");
-  }
-  console.log("Allowed, you may continue.");
-  next();
-};
-
 const handleHome = (req, res) => {
   return res.send("I love middlewares");
 };
 
-const handleProtected = (req, res) => {
-  return res.send("Welcome to the private lounge.");
-};
-
-app.use(logger);
-app.use(privateMiddleware);
-
-app.get("/", handleHome);
-app.get("/protected", handleProtected);
+app.get("/", logger, handleHome);
 
 const handleListening = () =>
   console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
