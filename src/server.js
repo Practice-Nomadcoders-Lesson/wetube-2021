a9ec47bafb18e99d5c24fd393bb86b87cb6dfd78
iinsue <hinsu93@naver.com>
2022-02-03 15:30:18 +0900

    3.5 Middlewares part One

--- src/server.js
+++ src/server.js
@@ -4,16 +4,16 @@ const PORT = 4000;
 
 const app = express();
 
+const gossipMiddleware = (req, res, next) => {
+  console.log(`Someone is going to: ${req.url}`);
+  next();
+};
+
 const handleHome = (req, res) => {
-  return res.end();
+  return res.send("I love middlewares");
 };
 
-const handleLogin = (req, res) => {
-  return res.send("Login here.");
-};
-
-app.get("/", handleHome);
-app.get("/login", handleLogin);
+app.get("/", gossipMiddleware, handleHome);
 
 const handleListening = () =>
   console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
