fac3c845ac1dbfbdc243e9b80b181e02104e145d
iinsue <hinsu93@naver.com>
2022-02-05 05:08:13 +0900

    5.8 Iteration

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -3,8 +3,10 @@ const fakeUser = {
   loggedIn: true,
 };
 
-export const trending = (req, res) =>
-  res.render("home", { pageTitle: "Home", fakeUser: fakeUser });
+export const trending = (req, res) => {
+  const videos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  res.render("home", { pageTitle: "Home", videos });
+};
 export const see = (req, res) => res.render("watch");
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
