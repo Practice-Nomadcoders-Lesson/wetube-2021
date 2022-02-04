796a8c89f081874e99ac856fdc538874ec9fbd7b
iinsue <hinsu93@naver.com>
2022-02-04 16:14:04 +0900

    4.7 URL Parameters part One

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,12 @@
 export const trending = (req, res) => res.send("Home Page Videos");
-export const see = (req, res) => res.send("Watch Videos");
-export const edit = (req, res) => res.send("Edit Videos");
+export const see = (req, res) => {
+  return res.send(`Watch Videos #${req.params.id}`);
+};
+export const edit = (req, res) => {
+  return res.send("Edit Videos");
+};
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
-export const deleteVideo = (req, res) => res.send("Delete Video");
+export const deleteVideo = (req, res) => {
+  return res.send("Delete Video");
+};
