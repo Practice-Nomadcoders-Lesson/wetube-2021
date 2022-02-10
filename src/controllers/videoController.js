d0a1f376c9cdbc4c0fed34c61faca5704a449580
iinsue <hinsu93@naver.com>
2022-02-10 19:58:16 +0900

    6.15 Creating a Video part One

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -7,13 +7,13 @@ Video.find({}, (error,videos) => {
   return res.render("home", { pageTitle: "Home", videos});
 });
 */
-export const home = async(req, res) => {
-try{
-  const videos = await Video.find({});
-  return res.render("home", { pageTitle: "Home", videos});
-} catch {
-  return res.render("server-error");
-}
+export const home = async (req, res) => {
+  try {
+    const videos = await Video.find({});
+    return res.render("home", { pageTitle: "Home", videos });
+  } catch {
+    return res.render("server-error");
+  }
 };
 export const watch = (req, res) => {
   const { id } = req.params;
@@ -32,6 +32,18 @@ export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
 export const postUpload = (req, res) => {
-  const { title } = req.body;
+  const { title, description, hashtags } = req.body;
+  const video = new Video({
+    //video model의 구성요소
+    title,
+    description,
+    createdAt: Date.now(),
+    hashtags: hashtags.split(",").map((word) => `#${word}`),
+    meta: {
+      views: 0,
+      rating: 0,
+    },
+  });
+  console.log(video);
   return res.redirect("/");
 };
