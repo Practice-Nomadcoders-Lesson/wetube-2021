9e0b70afc7f7273a0634b1e6b912dd390ac27272
iinsue <hinsu93@naver.com>
2022-02-17 10:55:10 +0900

    7.14 Expiration and Secrets

--- src/server.js
+++ src/server.js
@@ -19,10 +19,10 @@ app.use(express.urlencoded({ extended: true }));
 //session middleware
 app.use(
   session({
-    secret: "Hello!",
+    secret: process.env.COOKIE_SECRET,
     resave: false,
     saveUninitialized: false,
-    store: MongoStore.create({ mongoUrl: "mongodb://127.0.0.1:27017/wetube" }),
+    store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
   })
 );
 
