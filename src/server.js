a3031e214d6048dd5d19250770856df26db2cfc5
iinsue <hinsu93@naver.com>
2022-02-17 10:05:02 +0900

    7.13 Uninitialized Sessions

--- src/server.js
+++ src/server.js
@@ -20,9 +20,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(
   session({
     secret: "Hello!",
-    resave: true,
-    saveUninitialized: true,
-    store: MongoStore.create({mongoUrl: "mongodb://127.0.0.1:27017/wetube"}),
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: "mongodb://127.0.0.1:27017/wetube" }),
   })
 );
 
