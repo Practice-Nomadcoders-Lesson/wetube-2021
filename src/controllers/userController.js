1ccf7aeb0a4878b9781a4d0cccc6839f11e10040
iinsue <hinsu93@naver.com>
2022-02-18 20:18:23 +0900

    8.3 Edit Profile POST part Two

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -147,12 +147,17 @@ export const postEdit = async (req, res) => {
     },
     body: { name, email, username, location },
   } = req;
-  await User.findByIdAndUpdate(_id, {
-    name,
-    email,
-    username,
-    location,
-  });
-  return res.render("edit-profile");
+  const updateUser = await User.findByIdAndUpdate(
+    _id,
+    {
+      name,
+      email,
+      username,
+      location,
+    },
+    { new: true }
+  );
+  req.session.user = updateUser;
+  return res.redirect("/users/edit");
 };
 export const see = (req, res) => res.send("See User");
