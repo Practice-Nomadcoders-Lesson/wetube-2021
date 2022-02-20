34f16fe7d180cd927edcd0421367c7e224487b42
iinsue <hinsu93@naver.com>
2022-02-20 23:27:51 +0900

    8.9 Video Upload

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -42,12 +42,14 @@ export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
 export const postUpload = async (req, res) => {
+  const { path: fileUrl } = req.file;
   const { title, description, hashtags } = req.body;
   try {
     await Video.create({
       //video model의 구성요소
       title,
       description,
+      fileUrl,
       hashtags: Video.formatHashtags(hashtags),
     });
     return res.redirect("/");
