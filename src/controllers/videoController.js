a49f121a22bdce994fe771ce17fd3b0ff5a04ace
iinsue <hinsu93@naver.com>
2022-02-23 00:15:10 +0900

    10.1 Styles part One

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -3,7 +3,9 @@ import Video from "../models/Video";
 
 export const home = async (req, res) => {
   try {
-    const videos = await Video.find({}).sort({ createdAt: "desc" });
+    const videos = await Video.find({})
+      .sort({ createdAt: "desc" })
+      .populate("owner");
     return res.render("home", { pageTitle: "Home", videos });
   } catch {
     return res.render("server-error");
