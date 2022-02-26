77a7880621dafdf7ae69f3c142fd15a2d7ecc790
iinsue <hinsu93@naver.com>
2022-02-27 01:31:44 +0900

    16.5 Commenting

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,5 @@
 import User from "../models/User";
+import Comment from "../models/Comment";
 import Video from "../models/Video";
 
 export const home = async (req, res) => {
@@ -131,8 +132,24 @@ export const registerView = async (req, res) => {
   return res.sendStatus(200);
 };
 
-export const createComment = (req, res) => {
-  console.log(req.params);
-  console.log(req.body);
-  return res.end();
+export const createComment = async (req, res) => {
+  const {
+    session: { user },
+    body: { text },
+    params: { id },
+  } = req;
+
+  const video = await Video.findById(id);
+
+  if (!video) {
+    return res.sendStatus(404);
+  }
+
+  const comment = await Comment.create({
+    text,
+    owner: user._id,
+    video: id,
+  });
+
+  return res.sendStatus(201);
 };
