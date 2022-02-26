d99c1ea898b9fc074c21d09008f942501eb5c189
iinsue <hinsu93@naver.com>
2022-02-27 02:12:37 +0900

    16.6 Rendering Comments

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,7 +1,7 @@
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
 
-const handleSubmit = (event) => {
+const handleSubmit = async (event) => {
   const textarea = form.querySelector("textarea");
   event.preventDefault();
   const text = textarea.value;
@@ -9,7 +9,7 @@ const handleSubmit = (event) => {
   if (text === "") {
     return;
   }
-  fetch(`/api/videos/${videoId}/comment`, {
+  await fetch(`/api/videos/${videoId}/comment`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -17,6 +17,7 @@ const handleSubmit = (event) => {
     body: JSON.stringify({ text }),
   });
   textarea.value = "";
+  window.location.reload();
 };
 if (form) {
   form.addEventListener("submit", handleSubmit);
